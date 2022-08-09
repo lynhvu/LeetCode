c536d57dd9eb378b973cb5a1d281644291188f18
lynhvu <72360495+lynhvu@users.noreply.github.com>
2022-08-09 17:39:31 -0500

    Time: 0 ms (100.00%), Space: 42.9 MB (19.45%) - LeetHub

--- 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.java
+++ 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.java
@@ -4,20 +4,29 @@ class Solution {
             return 0;
         } if (nums.length == 1) {
             return nums[0];
-        }
-        int start = 0;
-        int end = nums.length - 1;
-        while (start < end) {
-            int mid = (start + end) / 2;
-            if (mid > 0 && nums[mid] < nums[mid - 1]) {
-                return nums[mid];
+        } 
+        
+        // want to keep track of start and end points 
+        // a res var to keep track of smallest val
+        int res = nums[0];
+        int start = 0; int end = nums.length - 1;
+        while (start <= end) {
+            
+            if (nums[end] > nums[start]) { // this mean the arr already sorted, return the min at the beginning 
+                res = Math.min(res, nums[start]);
+                return res;
             }
-            if (nums[start] <= nums[mid] && nums[mid] > nums[end]) {
+            int mid = (start + end) / 2;
+            res = Math.min(res, nums[mid]);
+            if (nums[mid] >= nums[start]) { 
+                // mid is a part of right arr, we move to the right
                 start = mid + 1;
             } else {
+                // mid is a part of left arr, cut that part and check the right
                 end = mid - 1;
             }
         }
-        return nums[start];
+        
+        return res;
     }
 }
